d877bbaca69643bf07a2e257b5cacc699040940f
muniru dreamer <munirudreamer@gmail.com>
2024-08-17 14:47:10 +0000

    Full Proto-type

---
+++ about_us.php
@@ -0,0 +1,144 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</head>
+<style>
+  .blood_compa img{
+    margin-left: -80px;
+  }
+  .about_us img{
+    width: 100%;
+    height: 100px;
+  }
+  .banner_img img{
+    width: 100%;
+    height: 400px;
+    margin-bottom: 30px;
+  }
+</style>
+<body>
+
+
+<?php 
+$active ='about';
+include('head.php');
+
+?>
+
+
+<div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+  <div class="container">
+    <div class="banner_img">
+      <img src="image/banner15.jpeg" alt="banner image">
+    </div>
+  <div id="content-wrap" style="padding-bottom:50px;">
+    <!-- section tips -->
+    <h1 style="text-align:center;font-size:45px;">Welcome to BloodBank & Donor Management System</h1>
+<br>
+        <div class="row">
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header card bg-info text-white" >The need for blood</h4>
+
+                        <p class="card-body overflow-auto" style="padding-left:2%;height:120px;text-align:left;">
+                          <?php
+                            include 'conn.php';
+                            $sql=$sql= "select * from pages where page_type='needforblood'";
+                            $result=mysqli_query($conn,$sql);
+                            if(mysqli_num_rows($result)>0)   {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                  echo $row['page_data'];
+                                }
+                              }
+
+                           ?>
+                         </p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header card bg-info text-white">Blood Tips</h4>
+
+                    <p class="card-body overflow-auto" style="padding-left:2%;height:120px;text-align:left;">
+                      <?php
+                        include 'conn.php';
+                        $sql=$sql= "select * from pages where page_type='bloodtips'";
+                        $result=mysqli_query($conn,$sql);
+                        if(mysqli_num_rows($result)>0)   {
+                            while($row = mysqli_fetch_assoc($result)) {
+                              echo $row['page_data'];
+                            }
+                          }
+
+                       ?>
+                     </p>
+
+                        </div>
+            </div>
+            <div class="col-lg-4 mb-4">
+                <div class="card">
+                    <h4 class="card-header card bg-info text-white" >Who you could Help</h4>
+
+                    <p class="card-body overflow-auto" style="padding-left:2%;height:120px;text-align:left;">
+                      <?php
+                        include 'conn.php';
+                        $sql=$sql= "select * from pages where page_type='whoyouhelp'";
+                        $result=mysqli_query($conn,$sql);
+                        if(mysqli_num_rows($result)>0)   {
+                            while($row = mysqli_fetch_assoc($result)) {
+                              echo $row['page_data'];
+                            }
+                          }
+                       ?>
+                     </p>
+
+
+                      </div>
+            </div>
+</div>
+
+    <!--End of section tips  -->
+
+<!-- blood compatibility -->
+ <div class="blood_compa">
+      <h1>Blood Compatibility</h1>
+      <img src="image/blood_compatability.jpeg" style="height:500px" alt="Error" >
+ </div>
+
+<div class="row">
+    <div class="col-lg-6">
+        <h1 class="mt-4 mb-3">About Us</h1>
+        <p> <?php
+          include 'conn.php';
+          $sql=$sql= "select * from pages where page_type='aboutus'";
+          $result=mysqli_query($conn,$sql);
+          if(mysqli_num_rows($result)>0)   {
+              while($row = mysqli_fetch_assoc($result)) {
+                echo $row['page_data'];
+              }
+            }
+
+         ?>
+      </p>
+
+    </div>
+    <div class="col-lg-6 about_us">
+        <img class="img-fluid rounded" src="image\_107317099_blooddonor976.jpg" style="height:400px" alt="error"  >
+    </div>
+</div>
+</div></div>
+
+<?php include('footer.php')
+?>
+</div>
+</body>
+
+</html>
